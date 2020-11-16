5b6413424085e51ef5746592f78ff4a7ad99068c
karen2 <karen2@karen2s-MacBook-Pro.local>
2020-11-15 20:02:49 -0500

    update

--- Laboratories/Lab4/Numerov-Laguerre_control.R
+++ Laboratories/Lab4/Numerov-Laguerre_control.R
@@ -3,14 +3,14 @@
 #Domain (x-axis):
 dr    <- 0.1                            # Increment on r-axis
 r.min <- 1e-15                          # Start of r-axis.
-r.max <- 180                             # End of r-axis.
+r.max <- 20                             # End of r-axis.
 r     <- seq(from=r.min,to=r.max,by=dr) # r-axis
 
 #Numerov's procedure to solve the (almost) Radial Schrodinger Equation:
-n            <- 6      # n = 1,2,3,...
-l            <- 4      # l = 0, ... , n-1 (e.g. s,p,d,f,g,.....)
+n            <- 1      # n = 1,2,3,...
+l            <- 0      # l = 0, ... , n-1 (e.g. s,p,d,f,g,.....)
 Guess.Energy <- (-0.5) # Initial guess for energy of the state
-max.iter     <- 50                  
+max.iter     <- 40                  
 state        <- (n-l-1)                  
 Fr.info      <- numerov.procedure(r,dr,"radial",state,Guess.Energy,max.iter,0.00)
 
@@ -23,4 +23,7 @@ plot(r, r^2 * psi.info[,2],typ="l")
 Npsi.info<-approx.normalize(psi.info, include.jacobian = TRUE)
 
 #Plot the normalized wave function:
-plot(r, r^2*Npsi.info[,2], xlim=c(min(r),max(r)),ylab="N psi(x)",typ="l")
+plot(r, Npsi.info[,2], xlim=c(min(r),max(r)),ylab="N psi(x)",typ="l")
+
+#Plot the normalized radial density:
+plot(r, r^2 * Npsi.info[,2]^2, xlim=c(min(r),max(r)),ylab="r^2 (N psi(x))^2",typ="l")
