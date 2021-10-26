140be46cbdde9ad5233d455a04e889b57c274a50
npetraco <npetraco@gmail.com>
2021-10-26 08:42:07 -0400

    update

--- Laboratories/Lab4/Numerov-Laguerre_control.R
+++ Laboratories/Lab4/Numerov-Laguerre_control.R
@@ -1,4 +1,4 @@
-#source(file.choose())
+library(che302r)
 
 #Domain (x-axis):
 dr    <- 0.1                            # Increment on r-axis
@@ -7,8 +7,8 @@ r.max <- 20                             # End of r-axis.
 r     <- seq(from=r.min,to=r.max,by=dr) # r-axis
 
 #Numerov's procedure to solve the (almost) Radial Schrodinger Equation:
-n            <- 1      # n = 1,2,3,...
-l            <- 0      # l = 0, ... , n-1 (e.g. s,p,d,f,g,.....)
+n            <- 2      # n = 1,2,3,...
+l            <- 1      # l = 0, ... , n-1 (e.g. s,p,d,f,g,.....)
 Guess.Energy <- (-0.5) # Initial guess for energy of the state
 max.iter     <- 40                  
 state        <- (n-l-1)                  
@@ -17,7 +17,7 @@ Fr.info      <- numerov.procedure(r,dr,"radial",state,Guess.Energy,max.iter,0.00
 #Transform F(r) back into R(r), the proper radial wave function
 psi.info     <- Fr.info
 psi.info[,2] <- (r^-1)*Fr.info[,2]
-plot(r, r^2 * psi.info[,2],typ="l")
+plot(r, psi.info[,2],typ="l")
 
 #Approximately Normalize Psi:
 Npsi.info<-approx.normalize(psi.info, include.jacobian = TRUE)
