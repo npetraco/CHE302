dae0c7255a14f3d107e5323f617740d5ec131fec
npetraco <npetraco@gmail.com>
2021-11-17 21:23:30 -0500

    update

--- Laboratories/Lab6/FT_lab_control.R
+++ Laboratories/Lab6/FT_lab_control.R
@@ -2,13 +2,19 @@ library(che302r)
 
 # Load the interferogram:
 dat <- read.csv("https://raw.githubusercontent.com/npetraco/CHE302/master/Laboratories/Lab6/interferogram.csv", header=T)
-dat <- as.matrix(dat)
+interferogram <- dat[,2]
 
 # Plot the interferogram:
-plot(1:length(dat[,1]), dat[,1], typ="l")
+plot(1:length(interferogram), interferogram, typ="l")
+plot(interferogram[2400:2600], typ="l") # Zoom
 
 # Fourier transform the interfrogram:
-spect<-fft.interferogram2(dat, plot.typ="%T")
-spect<-fft.interferogram2(dat, plot.typ="Absorbance")
+spect<-fft.interferogram2(interferogram, plot.typ="%T")
+spect<-fft.interferogram2(interferogram, plot.typ="Absorbance")
 
 # What molecule could this be?
+head(spect[[1]])
+plot(spect[[2]])
+plot(spect[[2]][,2])
+peak.idxs <- which(spect[[2]][,2] > 1)
+spect[[2]][peak.idxs,1]
