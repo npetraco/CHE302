0a31601c89deb09c8a49ea717b9edbc70a8bfebe
npetraco <npetraco@gmail.com>
2025-02-17 13:45:15 -0500

    update

--- Problem_Sets/Problem_Set.R
+++ Problem_Sets/Problem_Set.R
@@ -3,32 +3,32 @@ library(che302r)
 # 1.
 lam <- 3e-3
 pN  <- h/lam
-pN
+pN # momentum of neutron
 
 vN <- pN/mN
-vN
+vN # speed of neutron
 
 
 # 2.
 lam <- 350e-9
 pph <- h/lam
-pph
+pph # momentum of photon
 
 mH2 <- 2*mP + 2*me
-mH2
+mH2 # mass of hydrogen molecule
 
-pH2 <- pph
+pH2 <- pph     # hydrogen molecule momentum is same as photon momentum
 vH2 <- pH2/mH2
-vH2
+vH2            # speed of hydrogen molecule
 
 
 # 3.
 dv <- 1000
 dp <- mP*dv
-dp
+dp # uncertainty in momentum
 
 dx <-  hb/(2*dp)
-dx
+dx # uncertainty in position
 
 
 # 4.
@@ -106,27 +106,51 @@ pelec
 
 
 # 9.
-dx <- 150e-12
-dp <- hb/(2*dx)
-dp
+n      <- 4            # Fringe index
+d      <- 0.5e-3       # Slit spacing
+theta  <- 3.3 * pi/180 # Angle
 
-dv <- dp/me
-dv
+# a. n lambda = d sin(theta)
+lambda <- d*sin(theta)/n
+lambda
+lambda*1e9 # wavelength in nm
+
+# b. E = hc/lambda
+E <- h*cl/lambda
+E # energy in J
+
+# c. p = h/lambda
+p <- h/lambda
+p # momentum in kg m/s
+
+# d. p = mv
+v <- 100
+m <- p/v
+m # mass in kg
 
 
 # 10.
+dx <- 150e-12
+dp <- hb/(2*dx)
+dp              # minimum uncertainty in momentum
+
+dv <- dp/me
+dv              # minimum uncertainty in speed
+
+
+# 11.
 dt <- 1e-9
 dE <- hb/(2*dt)
 dE
 
 
-# 11.
+# 12.
 k100   <- planck.distribution(x.min = 0.1e-9, x.max = 200000e-9, Temp = 100, typ = "wavelength", plotQ = T)
 k1000  <- planck.distribution(x.min = 0.1e-9, x.max = 200000e-9, Temp = 1000, typ = "wavelength", plotQ = T)
 k10000 <- planck.distribution(x.min = 0.1e-9, x.max = 200000e-9, Temp = 10000, typ = "wavelength", plotQ = T)
 
 
-# 13.
+# 14.
 # a. True
 # b. False
 # c. False
