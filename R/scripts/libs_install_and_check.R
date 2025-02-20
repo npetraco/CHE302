497560cc5bfa28dcd76e4a9c20c0ccff9bd358d1
npetraco <npetraco@gmail.com>
2025-02-20 09:16:23 -0500

    update

---
+++ R/scripts/libs_install_and_check.R
@@ -0,0 +1,11 @@
+install.packages("remotes")
+library(remotes)
+install_github("npetraco/che302r")
+library(che302r)
+h # Planck's constant get spit out?
+
+# Optional packages:
+#install.packages("rgl")
+#install.packages("doMC")
+#install.packages("doSNOW")
+#install.packages("readJDX")
